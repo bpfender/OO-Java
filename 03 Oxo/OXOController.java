ae962c98b99b2c1249c67f4bd3c2770e30044915
bpfender <benedict.pfender@gmail.com>
2020-02-17 13:52:42 +0000

    Cleaned things up a bit

--- 03 Oxo/OXOController.java
+++ 03 Oxo/OXOController.java
@@ -17,13 +17,18 @@ class OXOController {
     public void handleIncomingCommand(String command) throws InvalidCellIdentifierException,
             CellAlreadyTakenException, CellDoesNotExistException {
 
+        if (command.length() < 2) {
+            throw new InvalidCellIdentifierException("location must be 2 chars,", command);
+        }
+
         // QUESTION how does exception handling work in view?
         if (!isCommand(command)) {
             // QUESTION what do these refer to?
-            throw new InvalidCellIdentifierException(command, command);
+            throw new InvalidCellIdentifierException("string must be number and letter", command);
         }
 
-        // QUESTION Does this make more sense as class vars? how do i determine what is local and
+        // QUESTION Does this make more sense as class vars? how do i determine what is
+        // local and
         // what isn;t?
         int rowNum = Character.toLowerCase(command.charAt(0)) - 'a';
         int colNum = Character.getNumericValue(command.charAt(1)) - 1;
@@ -38,19 +43,23 @@ class OXOController {
 
         model.setCellOwner(rowNum, colNum, model.getCurrentPlayer());
 
-        if (++turn >= model.getNumberOfRows() * model.getNumberOfColumns()) {
-            model.setGameDrawn();
-        } else if (rounds >= model.getWinThreshold() - 1) {
+        if (rounds >= model.getWinThreshold() - 1) {
             if (checkForWin(rowNum, colNum)) {
                 model.setWinner(model.getCurrentPlayer());
             }
         }
 
+        if (++turn >= model.getNumberOfRows() * model.getNumberOfColumns()) {
+            model.setGameDrawn();
+        }
+
         setNextPlayer();
     }
 
-    // Check the structure of the input, ensuring single letter followed by single digit. Could be
+    // Check the structure of the input, ensuring single letter followed by single
+    // digit. Could be
     // expanded for huge grids, but unclear what preferred
+    // QUESTION do you want this to work for 10,11,12 etc?
     private boolean isCommand(String command) {
         if (command.matches("^(([a-z]|[A-Z])[0-9])")) {
             return true;
@@ -70,67 +79,32 @@ class OXOController {
     }
 
     private boolean checkForWin(int rowNum, int colNum) {
-        if (checkHorizontal(rowNum, colNum) || checkVertical(rowNum, colNum)
-                || checkUpDiagonal(rowNum, colNum) || checkDownDiagonal(rowNum, colNum)) {
+        if (checkWinVector(rowNum, colNum, 1, 0) || checkWinVector(rowNum, colNum, 0, 1)
+                || checkWinVector(rowNum, colNum, 1, 1) || checkWinVector(rowNum, colNum, 1, -1)) {
             return true;
         }
+
         return false;
     }
 
-    private boolean checkHorizontal(int rowNum, int colNum) {
+    private boolean checkWinVector(int rowNum, int colNum, int dir_x, int dir_y) {
         int count = 1;
 
-        while (checkCellOwner(rowNum, ++colNum)) {
-            count++;
-        }
-        colNum -= count;
-        while (checkCellOwner(rowNum, --colNum)) {
-            count++;
-        }
+        int col = colNum + dir_x;
+        int row = rowNum + dir_y;
 
-        return isWinThreshold(count);
-    }
-
-    private boolean checkVertical(int rowNum, int colNum) {
-        int count = 1;
-
-        while (checkCellOwner(++rowNum, colNum)) {
-            count++;
-        }
-        rowNum -= count;
-        while (checkCellOwner(--rowNum, colNum)) {
+        while (checkCellOwner(row, col)) {
+            col += dir_x;
+            row += dir_y;
             count++;
         }
 
-        return isWinThreshold(count);
-    }
+        col = colNum - dir_x;
+        row = rowNum - dir_y;
 
-    private boolean checkDownDiagonal(int rowNum, int colNum) {
-        int count = 1;
-
-        while (checkCellOwner(++rowNum, ++colNum)) {
-            count++;
-        }
-        rowNum -= count;
-        colNum -= count;
-
-        while (checkCellOwner(--rowNum, --colNum)) {
-            count++;
-        }
-
-        return isWinThreshold(count);
-    }
-
-    private boolean checkUpDiagonal(int rowNum, int colNum) {
-        int count = 1;
-
-        while (checkCellOwner(++rowNum, --colNum)) {
-            count++;
-        }
-        rowNum -= count;
-        colNum += count;
-
-        while (checkCellOwner(--rowNum, ++colNum)) {
+        while (checkCellOwner(row, col)) {
+            col -= dir_x;
+            row -= dir_y;
             count++;
         }
 
@@ -146,14 +120,16 @@ class OXOController {
     }
 
     private boolean isCell(int row, int col) {
-        // QUESTION as below, does it makes more sense to store row/col as attribute? more so here
+        // QUESTION as below, does it makes more sense to store row/col as attribute?
+        // more so here
         return 0 <= row && row < model.getNumberOfRows() && 0 <= col
                 && col < model.getNumberOfColumns();
     }
 
     // Checks whether threshold for player win has been reached
     private boolean isWinThreshold(int count) {
-        // QUESTION model.getWinThreshold() will be called many times. Does it make more sense to
+        // QUESTION model.getWinThreshold() will be called many times. Does it make more
+        // sense to
         // store as an attribute?
         return count >= model.getWinThreshold();
     }
