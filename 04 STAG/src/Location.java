90dd3ef01df424ecf3e20ce186978149b995a84b
bpfender <benedict.pfender@gmail.com>
2020-03-05 16:50:01 +0000

    Janky code to add entities to locations

--- 04 STAG/src/Location.java
+++ 04 STAG/src/Location.java
@@ -8,10 +8,26 @@ public class Location extends Entity {
         super(name, description);
     }
 
-    Map<String, Location> paths;
-    Map<String, Character> characters;
-    Map<String, Artefact> artefacts;
-    Map<String, Furniture> furniture;
-    Map<String, Player> players;
+    Map<String, Location> paths = new HashMap<>();
+    Map<String, Character> characters = new HashMap<>();
+    Map<String, Artefact> artefacts = new HashMap<>();
+    Map<String, Furniture> furniture = new HashMap<>();
+    Map<String, Player> players = new HashMap<>();
+
+    // FIXME this is super horrible
+    public <T extends Entity> void addEntity(T entity) {
+
+        if (entity instanceof Location) {
+            paths.put(entity.getName(), (Location) entity);
+        } else if (entity instanceof Character) {
+            characters.put(entity.getName(), (Character) entity);
+        } else if (entity instanceof Artefact) {
+            artefacts.put(entity.getName(), (Artefact) entity);
+        } else if (entity instanceof Furniture) {
+            furniture.put(entity.getName(), (Furniture) entity);
+        } else if (entity instanceof Player) {
+            players.put(entity.getName(), (Player) entity);
+        }
+    }
 
 }
