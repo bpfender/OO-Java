43d76ff239fc7cc216be01f4de68ecd2be6e1a2c
bpfender <benedict.pfender@gmail.com>
2020-03-06 19:25:06 +0000

    Added health feature

--- 04 STAG/src/Player.java
+++ 04 STAG/src/Player.java
@@ -4,10 +4,13 @@ import java.util.List;
  * Player
  */
 public class Player extends Character {
+    int health = 3;
+
+    List<Artefact> inventory;
+    Location currentLocations;
+
     public Player(String name, String description) {
         super(name, description);
     }
 
-    List<Artefact> inventory;
-    Location currentLocations;
 }
