8ed80855723b2d5d2bd856bb43ff07e73f9419bb
bpfender <benedict.pfender@gmail.com>
2020-03-14 23:53:07 +0000

    Renaming to make more sense

---
+++ 04 STAG/src/Game.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+
+/**
+ * World
+ */
+// TODO should this be a singleton?
+
+public class Game {
+    private EntityMap<Location> locationMap = new EntityMap<>();
+    private EntityMap<Player> playerMap = new EntityMap<>();
+    private HashMap<String, Action> actions = new HashMap<String, Action>();
+    private Location startLocation;
+
+    // TODO seperate record for unplaced?
+
+    public void addEntity(Location entity) {
+        locationMap.addEntity(entity);
+    }
+
+    public void addEntity(Player entity) {
+        playerMap.addEntity(entity);
+    }
+
+    public EntityMap<Location> getLocationMap() {
+        return locationMap;
+    }
+
+    public EntityMap<Player> getPlayerMap() {
+        return playerMap;
+    }
+
+    public void addAction(String name, Action action) {
+        actions.put(name, action);
+    }
+
+    public Action getAction(String name) {
+        return actions.get(name);
+    }
+
+    public void setStartLocation(Location start) {
+        this.startLocation = start;
+    }
+
+    public Location getStartLocation() {
+        return startLocation;
+    }
+}
