f57b3bc7dda99b880cb7e9fe6c452ae67a96fd8e
bpfender <benedict.pfender@gmail.com>
2020-04-24 14:27:40 +0100

    Modified regex for parsing to stack

--- 05 Database/src/Parser/Parser.java
+++ 05 Database/src/Parser/Parser.java
@@ -1,56 +1,115 @@
 package Parser;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import Expression.*;
 
 public class Parser {
-    String query;
+    Stack<String> tokens = new Stack<>();
+    String error;
 
-    public Parser(String query) {
-        this.query = query.trim();
-    }
-
-    public String parseQuery() {
-        if (!checkTerminator()) {
-            return "ERROR: Semicolon missing from end of query";
+    public Expression parseQuery(String query) {
+        if (!query.endsWith(";")) {
+            error = "ERROR: Semicolon missing from end of query";
         }
 
-        return commandType();
+        tokenizeQuery(query);
+        return parseCommand();
     }
 
-    private boolean checkTerminator() {
-        if (query.endsWith(";")) {
-            query = query.substring(0, query.length() - 1);
-            return true;
+    private void tokenizeQuery(String query) {
+        Matcher m = Pattern.compile("[a-zA-Z_0-9]+|[(,)]|[><=!]+").matcher(query);
+        while (m.find()) {
+            tokens.push(m.group());
+        }
+    }
+
+    private Expression parseCommand() {
+        String token = tokens.pop();
+
+        if (tokens.empty()) {
+            error = "ERROR Expected more arguments";
+            return null;
         }
 
-        return false;
-    }
+        if (token.equals("use")) {
+            parseUse();
+        } else if (token.equals("create")) {
+            parseCreate();
+        } else if (token.equals("drop")) {
 
-    private String commandType() {
-        String[] splitQuery = query.split("\\s", 2);
+        } else if (token.equals("alter")) {
 
-        if (splitQuery[0].equals("use")) {
+        } else if (token.equals("insert")) {
 
-        } else if (splitQuery[0].equals("create")) {
+        } else if (token.equals("select")) {
 
-        } else if (splitQuery[0].equals("drop")) {
+        } else if (token.equals("update")) {
 
-        } else if (splitQuery[0].equals("alter")) {
+        } else if (token.equals("delete")) {
 
-        } else if (splitQuery[0].equals("insert")) {
-
-        } else if (splitQuery[0].equals("select")) {
-
-        } else if (splitQuery[0].equals("update")) {
-
-        } else if (splitQuery[0].equals("delete")) {
-
-        } else if (splitQuery[0].equals("join")) {
+        } else if (token.equals("join")) {
 
         } else {
-            return "ERROR";
+            error = "ERROR Invalid command";
+            return null;
         }
+
+    }
+
+    private Expression parseUse() {
+        String token = tokens.pop();
+
+        if (!tokens.empty()) {
+            error = "ERROR Unexpected arguments after name";
+            return null;
+        }
+        return new Use(token);
+    }
+
+    private Expression parseCreate() {
+        String token = tokens.pop();
+
+        if (tokens.empty()) {
+            error = "ERROR expected more arguments";
+            return null;
+        }
+
+        if (token.equals("table")) {
+            return parseCreateTable(splitQuery[1]);
+
+        } else if (token.equals("database")) {
+            return parseCreateDatabase(splitQuery[1]);
+        }
+
+        error = "ERROR Unexpected token";
         return null;
+    }
+
+    private Expression parseCreateDatabase(String remainingQuery) {
+        String[] splitQuery = remainingQuery.split("\\s", 2);
+
+        if (splitQuery.length > 1) {
+            error = "ERROR unexpected arguments at end of query";
+            return null;
+        }
+
+        return new CreateDatabase(splitQuery[0]);
+    }
+
+    private Expression parseCreateTable(String remainingQuery) {
+        String[] splitQuery = remainingQuery.split("\\s", 2);
+        List<String> attributeList = null;
+
+        if (splitQuery.length > 1) {
+
+        }
+
+        return new CreateTable(splitQuery[0], attributeList);
     }
 
 }
